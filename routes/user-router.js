fc8eafa3c2671f0617d9386be204caaf18fe41fb
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-20 14:26:09 -0500

    All user routes working correctly

--- routes/user-router.js
+++ routes/user-router.js
@@ -13,12 +13,74 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  if (req.body.name) {
+    try {
+      const user = await db.insert(req.body);
+      res.status(201).json(user);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong adding the user." });
+    }
+  } else {
+    res.status(400).json({ message: "Please provide a name for the user." });
+  }
+});
 
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await db.getById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "The user could not be located." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong fetching the user." });
+  }
+});
 
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  if (name) {
+    try {
+      const count = await db.update(id, req.body);
+      res.status(200).json(count);
+    } catch (error) {
+      res.status(404).json({ message: "The user could not be located." });
+    }
+  } else if (!name) {
+    res
+      .status(400)
+      .json({ message: "Please provide an updated name for the user." });
+  } else {
+    res
+      .status(500)
+      .json({ message: "Something went wrong trying to update the user." });
+  }
+});
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await db.remove(id);
+    if (count) {
+      res.status(200).json(count);
+    } else {
+      res.status(404).json({ message: "The user could not be located." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Something went wrong while trying to delete the user."
+      });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const userRouter = require("./routes/user-router");
 const postsRouter = require("./routes/posts-router");
 
 //Middleware
+function upperCaseName(req, res, next) {
+  if (req.body.name) {
+    req.body.name = req.body.name.toUpperCase();
+  }
+  next();
+}
+
 server.use(express.json());
+server.use(upperCaseName);
 
 //Routing Middleware
 server.use("/api/users", userRouter);
